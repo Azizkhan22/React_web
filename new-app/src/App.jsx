b5f9bf71a1872d9cc08b4a0b4dd5ba3f3179122d
Azizkhan22 <azizkhan22kakar@gmail.com>
2025-03-16 02:32:37 +0500

    Mini project of Travel jornal made with react and dynamic data

--- new-app/src/App.jsx
+++ new-app/src/App.jsx
@@ -1,13 +1,17 @@
-import Main from "./components/main"
 import NavBar from "./components/nav"
 import Entry from "./components/entry"
+import data from "./data.js"
+
 
 export default function App() {
+    const entryComponents = data.map(
+        obj => <Entry entry={obj}/>
+    )
+    
     return (
         <>
             <NavBar />
-            <Entry />
-            <Entry />
+            {entryComponents}
         </>
     )
 }
--- new-app/src/components/entry.jsx
+++ new-app/src/components/entry.jsx
@@ -2,18 +2,18 @@ export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="main-img-container">
-                <img className="main-img" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+                <img className="main-img" src={props.entry.img.src} alt={props.entry.img.alt} />
             </div>
             <div>
                 <div className="journal-top-container">
                     <img src="src/assets/marker.png" alt="map marker icon" />
-                    <span>Japan</span>
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+                    <span>{props.entry.country}</span>
+                    <a href={props.entry.map}>
                         View on Google Maps</a>
                 </div>
-                <h2 className="journal-place-title">Mount Fuji</h2>
-                <p className="journal-date">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="journal-description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h2 className="journal-place-title">{props.entry.title}</h2>
+                <p className="journal-date">{props.entry.dates}</p>
+                <p className="journal-description">{props.entry.text}</p>
             </div>
         </article>
     )
